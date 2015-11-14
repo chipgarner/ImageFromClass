1b976817fb594ac9227c2904f015c395f822c27c
chipgarner <chip@garnertotalenergy.com>
2015-11-14 11:13:33 +0800

    Refactored into class, uses CommonCaffe, works with MIT Places

--- ImageFromClass.py
+++ ImageFromClass.py
@@ -4,127 +4,104 @@ import scipy.ndimage as nd
 import PIL.Image
 import cv2
 
-import caffe
 
-# If your GPU supports CUDA and Caffe was built with CUDA support,
-# call the following to run Caffe operations on the GPU.
-def use_gpu():
-    caffe.set_mode_gpu()
-    caffe.set_device(0) # select GPU device if multiple devices exist
+class ImageFromClass:
+    def __init__(self):
+        pass
 
-def load_model():
-    model_path = 'models/' # substitute your path here
-    net_fn   = model_path + 'deploy_googlenet_updated.prototxt'
-    param_fn = model_path + 'bvlc_googlenet.caffemodel'
+    # def load_image(image_relative_path):
+    #     img = np.float32(PIL.Image.open(image_relative_path))
+    #     return img
 
-    net = caffe.Classifier(net_fn, param_fn,
-                        mean = np.float32([104.0, 116.0, 122.0]), # ImageNet mean, training set dependent
-                        channel_swap = (2,1,0)) # the reference model has channels in BGR order instead of RGB
+    # a couple of utility functions for converting to and from Caffe's input image layout
+    def __preprocess(self, net, img):
+        return np.float32(np.rollaxis(img, 2)[::-1]) - net.transformer.mean['data']
 
-    return net
+    def __deprocess(self, net, img):
+        return np.dstack((img + net.transformer.mean['data'])[::-1])
 
-def load_image(image_relative_path):
-    img = np.float32(PIL.Image.open(image_relative_path))
-    return img
+    def __name_result_class(self, imageNetClass):
+        return "output/" + 'Class ' + str(imageNetClass) + ".jpg"
 
-# a couple of utility functions for converting to and from Caffe's input image layout
-def preprocess(net, img):
-    return np.float32(np.rollaxis(img, 2)[::-1]) - net.transformer.mean['data']
-def deprocess(net, img):
-    return np.dstack((img + net.transformer.mean['data'])[::-1])
+    def __save_result(self, path, vis):
+        # adjust image contrast and clip
+        vis = vis * (255.0 / np.percentile(vis, 99.98))
+        vis = np.uint8(np.clip(vis, 0, 255))
 
-# def nameResult(index, end):
-#     return "output/" + str(index) + end.replace('/', '-') + ".jpg"
+        pimg = PIL.Image.fromarray(vis)
 
-def nameResultClass(imageNetClass):
-    return "output/" + 'Class ' + str(imageNetClass) + ".jpg"
+        pimg.save(path, 'jpeg')
 
+    def __show_result(self, vis):
+        # adjust image contrast and clip
+        vis = vis * (255.0 / np.percentile(vis, 99.98))
+        vis = np.uint8(np.clip(vis, 0, 255))
 
-def saveResult(path, vis):
-    # adjust image contrast and clip
-    vis = vis*(255.0/np.percentile(vis, 99.98))
-    vis = np.uint8(np.clip(vis, 0, 255))
+        pimg = PIL.Image.fromarray(vis)
+        pimg.show()
 
-    pimg = PIL.Image.fromarray(vis)
+        # showOpenCV(vis)
 
-    pimg.save(path, 'jpeg')
+    def __showOpenCV(self, image):
+        cv2.namedWindow("test", cv2.WND_PROP_FULLSCREEN)
+        cv2.setWindowProperty("test", cv2.WND_PROP_FULLSCREEN, cv2.cv.CV_WINDOW_FULLSCREEN)
+        bgr = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)  # I think this is being converted both ways ...
+        cv2.imshow("test", bgr)
+        cv2.waitKey(0)  # Scripting languages are weird, It will not display without this
+        cv2.destroyAllWindows()
 
+    def __blur(self, img, sigma):
+        if sigma > 0:
+            img[0] = nd.filters.gaussian_filter(img[0], sigma, order=0)
+            img[1] = nd.filters.gaussian_filter(img[1], sigma, order=0)
+            img[2] = nd.filters.gaussian_filter(img[2], sigma, order=0)
+        return img
 
-def showResult(vis):
-    # adjust image contrast and clip
-    vis = vis*(255.0/np.percentile(vis, 99.98))
-    vis = np.uint8(np.clip(vis, 0, 255))
+    def objective_L2(dst):
+        dst.diff[:] = dst.data
 
-    pimg = PIL.Image.fromarray(vis)
-    pimg.show()
+    def objective_L2_class(dst, imageNetClass=681):  # 366 gorrilla, 386 elephant, 834 suit, notebook 681, tandem 444
 
-    #showOpenCV(vis)
+        one_hot = np.zeros_like(dst.data)
+        one_hot.flat[imageNetClass] = 1.
+        dst.diff[:] = one_hot
 
+    def __make_step(self, net, netClass, end, sigma, step_size=3, objective=objective_L2_class):
+        '''Basic gradient ascent step.'''
 
-def showOpenCV(image):
-    cv2.namedWindow("test", cv2.WND_PROP_FULLSCREEN)
-    cv2.setWindowProperty("test", cv2.WND_PROP_FULLSCREEN, cv2.cv.CV_WINDOW_FULLSCREEN)
-    bgr = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)#I think this is being converted both ways ...
-    cv2.imshow("test", bgr)
-    cv2.waitKey(0)  # Scripting languages are weird, It will not display without this
-    cv2.destroyAllWindows()
+        src = net.blobs['data']  # input image is stored in Net's 'data' blob
+        dst = net.blobs[end]
 
+        net.forward(end=end)
+        objective(dst, netClass)  # specify the optimization objective
+        net.backward(start=end)
+        g = src.diff[0]
 
-def blur(img, sigma):
-    if sigma > 0:
-        img[0] = nd.filters.gaussian_filter(img[0], sigma, order=0)
-        img[1] = nd.filters.gaussian_filter(img[1], sigma, order=0)
-        img[2] = nd.filters.gaussian_filter(img[2], sigma, order=0)
-    return img
+        # apply normalized ascent step to the input image
+        src.data[:] += step_size / np.abs(g).mean() * g
 
-def objective_L2(dst):
-    dst.diff[:] = dst.data
+        src.data[0] = self.__blur(src.data[0], sigma)
 
-def objective_L2_class(dst, imageNetClass = 681):# 366 gorrilla, 386 elephant, 834 suit, notebook 681, tandem 444
+    ###################################################
 
-    one_hot = np.zeros_like(dst.data)
-    one_hot.flat[imageNetClass] = 1.
-    dst.diff[:] = one_hot
+    def deepdraw(self, net, base_img, octaves, netClass):
+        # reshape and load image
+        source = net.blobs['data']
+        h, w, c = base_img.shape[:]
+        source.reshape(1, 3, h, w)
+        source.data[0] = self.__preprocess(net, base_img)
 
-def make_step(net, netClass, end, sigma, step_size=3, objective=objective_L2_class):
-    '''Basic gradient ascent step.'''
+        for e, o in enumerate(octaves):
+            layer = o['layer']
 
-    src = net.blobs['data'] # input image is stored in Net's 'data' blob
-    dst = net.blobs[end]
+            for i in xrange(o['iter_n']):
+                sigma = o['start_sigma'] + ((o['end_sigma'] - o['start_sigma']) * i) / o['iter_n']
+                step_size = o['start_step_size'] + ((o['end_step_size'] - o['start_step_size']) * i) / o['iter_n']
 
-    net.forward(end=end)
-    objective(dst, netClass)  # specify the optimization objective
-    net.backward(start=end)
-    g = src.diff[0]
+                self.__make_step(net, netClass, end=layer, sigma=sigma, step_size=step_size)
 
-    # apply normalized ascent step to the input image
-    src.data[:] += step_size/np.abs(g).mean() * g
+        # visualization
+        vis = self.__deprocess(net, source.data[0])
+        self.__show_result(vis)
 
-    src.data[0] = blur(src.data[0], sigma)
-
-###################################################
-
-def deepdraw(net, base_img, octaves, netClass, **step_params):
-    # reshape and load image
-    source = net.blobs['data']
-    h, w, c = base_img.shape[:]
-    source.reshape(1,3,h,w)
-    source.data[0] = preprocess(net, base_img)
-
-    vis = deprocess(net, source.data[0])
-    # showResult(vis)
-
-    for e,o in enumerate(octaves):
-        layer = o['layer']
-
-        for i in xrange(o['iter_n']):
-            sigma = o['start_sigma'] + ((o['end_sigma'] - o['start_sigma']) * i) / o['iter_n']
-            step_size = o['start_step_size'] + ((o['end_step_size'] - o['start_step_size']) * i) / o['iter_n']
-
-            make_step(net, netClass, end=layer, sigma=sigma, step_size=step_size)
-
-    # visualization
-    vis = deprocess(net, source.data[0])
-    #showResult(vis)
-
-    saveResult(nameResultClass(netClass), vis)
+        self.__save_result(self.__name_result_class(netClass), vis)
--- runner.py
+++ runner.py
@@ -1,9 +1,7 @@
 import ImageFromClass as ifc
 import numpy as np
-import PIL.Image
-
-ifc.use_gpu()
-net = ifc.load_model()
+import setup_caffe_network as su
+import models as ml
 
 octaves = [
     {
@@ -34,41 +32,55 @@ octaves = [
     },
     {
         'layer':'loss1/classifier',
-        'iter_n':10,
+        'iter_n':50,
         'start_sigma':0.44,
         'end_sigma':0.304,
         'start_step_size':3.,
         'end_step_size':3.
     }
 ]
-
+#
 # octaves = [
 #     {
 #         'layer':'loss3/classifier',
 #         'iter_n':190,
-#         'start_sigma':2.5,
-#         'end_sigma':0.78,
+#         'start_sigma':1.0,
+#         'end_sigma':0.73,
 #         'start_step_size':11.,
-#         'end_step_size':11.
+#         'end_step_size':6.
 #     },
 #     {
 #         'layer':'loss3/classifier',
 #         'scale':1.2,
 #         'iter_n':450,
-#         'start_sigma':0.78*1.2,
-#         'end_sigma':0.40,
+#         'start_sigma':0.0,
+#         'end_sigma':0.0,
 #         'start_step_size':6.,
 #         'end_step_size':3.
 #     }
 # ]
 
-#Change the color palette. 100, 100, 50 makes more greens and browns, less blues and whites
-#lower numbers give darker colors
-background_color = np.float32([100.0, 100.0, 100.0])
+### For testing
+# octaves = [
+#     {
+#         'layer': 'loss3/classifier',
+#         'iter_n': 10,
+#         'start_sigma': 2.5,
+#         'end_sigma': 0.78,
+#         'start_step_size': 11.,
+#         'end_step_size': 3.
+#     }
+# ]
+
+su.SetupCaffe.gpu_on()
+net = ml.NetModels.setup_places_model()
+
+# Change the color palette. 100, 100, 50 makes more greens and browns, less blues and whites
+# lower numbers give darker colors
+background_color = np.float32([100.0, 100.0, 75.0])
 # generate initial random image
 gen_image = np.random.normal(background_color, .001, (228, 228, 3))
 
-#????img = np.float32(PIL.Image.open('youandmeSmall.jpg'))
-
-for i in range(4, 1000):
-    ifc.deepdraw(net, gen_image, octaves, i)
+IFC = ifc.ImageFromClass()
+for i in range(0, 2):
+    IFC.deepdraw(net, gen_image, octaves, i)
